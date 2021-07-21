6b71b403d0cd0609cce95b07c2958ad6ff057f48
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2021-07-21 18:30:34 -0300

    prepare for prod

--- mobilidade_rio/mobilidade_rio/settings/base.py
+++ mobilidade_rio/mobilidade_rio/settings/base.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = Path(__file__).resolve().parent / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 
 # Quick-start development settings - unsuitable for production
