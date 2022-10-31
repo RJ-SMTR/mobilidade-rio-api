9843c753c88c8d7c99417117de6f173779ad7aa8
Raphael Rivas <raphaelrivas@hotmail.com>
2022-10-31 18:21:25 -0300

    dev django configs
    For now it doesnt use .env (TODO)

--- mobilidade_rio/mobilidade_rio/settings/dev.py
+++ mobilidade_rio/mobilidade_rio/settings/dev.py
@@ -1,3 +1,16 @@
 from .base import *
+from os import getenv
 
 ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    # TODO: usar variáveis globais quando o Docker conseguir definir
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "postgres",  #getenv("DB_NAME"),
+        'USER': "postgres",  #getenv("DB_USER"),
+        'PASSWORD': "postgres",  #getenv("DB_PASSWORD"),
+        'HOST': "db",  #getenv("DB_HOST"),
+        'PORT': 5432,  #getenv("DB_PORT"),
+    },
+}
