0a1fa7a84be2a4746d71d8834ed8e6278dd97d71
Raphael Rivas <raphaelrivas@hotmail.com>
2022-11-08 15:38:22 -0300

    using utils to pass env or use default values

--- mobilidade_rio/mobilidade_rio/settings/dev.py
+++ mobilidade_rio/mobilidade_rio/settings/dev.py
@@ -1,16 +1,16 @@
 from .base import *
-from os import getenv
+from .utils import get_db_envs
 
 ALLOWED_HOSTS = ["*"]
 
-DATABASES = {
-    # TODO: usar variáveis globais quando o Docker conseguir definir
+# remove envs if they are 
+
+db_values = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "postgres",  #getenv("DB_NAME"),
-        'USER': "postgres",  #getenv("DB_USER"),
-        'PASSWORD': "postgres",  #getenv("DB_PASSWORD"),
-        'HOST': "db",  #getenv("DB_HOST"),
-        'PORT': 5432,  #getenv("DB_PORT"),
     },
 }
+# insert envs or default values
+db_values["default"].update(get_db_envs())
+
+DATABASES = db_values
