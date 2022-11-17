419ad99a562f1a668a4828f7484674a1d190d238
Raphael Rivas <raphaelrivas@hotmail.com>
2022-11-17 16:59:41 -0300

    urls for pontos are now '<site>/gtfs/<entity>'

--- mobilidade_rio/mobilidade_rio/urls.py
+++ mobilidade_rio/mobilidade_rio/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 from django.urls import include, path
 from django.contrib import admin
-from django.urls import path
 from rest_framework import routers
 
 from mobilidade_rio.pontos import views
@@ -21,7 +20,7 @@ router.register(r"frequencies", views.FrequenciesViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path("", include(router.urls)),
+    path("gtfs/", include(router.urls)),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     url(r"^auth/", include("djoser.urls")),
