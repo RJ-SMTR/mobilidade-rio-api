55b67329c8aee32ac7ef1df10564fb2b6ce5bdf6
Raphael Rivas <raphaelrivas@hotmail.com>
2023-01-10 02:26:06 -0300

    rename ShapesWithStops in serializers

--- mobilidade_rio/mobilidade_rio/predictor/serializers.py
+++ mobilidade_rio/mobilidade_rio/predictor/serializers.py
@@ -2,9 +2,7 @@ from mobilidade_rio.predictor.models import *
 from rest_framework import serializers
 
 
-class ShapeWithStopsSerializer(serializers.HyperlinkedModelSerializer):
+class ShapeWithStopsSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ShapeWithStops
+        model = ShapesWithStops
         fields = "__all__"
-
-
