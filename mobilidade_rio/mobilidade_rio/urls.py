5e7dd63e69f2a54e46049635d7a18e430db51c09
Raphael Rivas <raphaelrivas@hotmail.com>
2023-01-25 17:13:09 -0300

    remove sws, add predictor endpoints

--- mobilidade_rio/mobilidade_rio/urls.py
+++ mobilidade_rio/mobilidade_rio/urls.py
@@ -18,8 +18,8 @@ gtfs_router.register(r"stop_times", gtfs.StopTimesViewSet, basename="stop_times"
 gtfs_router.register(r"frequencies", gtfs.FrequenciesViewSet)
 
 pred_router = routers.DefaultRouter()
-pred_router.register(r"shape_with_stops", pred.ShapeWithStopsViewSet,basename="shape_with_stops")
-#pred_router.register(r"predictor", pred.PredictorViewSet,basename="pred_arrivals")
+# pred_router.register(r"shape_with_stops", pred.ShapeWithStopsViewSet,basename="shape_with_stops")
+pred_router.register(r"predictor", pred.PredictorViewSet,basename="pred_arrivals")
 
 unified_router = routers.DefaultRouter()
 # add all gtfs_router and pred_router to unified_router
