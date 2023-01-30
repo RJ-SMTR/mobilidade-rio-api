ce546353f949d0290496ff17004d79a938e1af49
Raphael Rivas <raphaelrivas@hotmail.com>
2023-01-30 17:00:26 -0300

    rename to stop_id_destiny

--- mobilidade_rio/mobilidade_rio/predictor/views.py
+++ mobilidade_rio/mobilidade_rio/predictor/views.py
@@ -18,7 +18,7 @@ class PredictorViewSet(viewsets.ModelViewSet):
     """
 
     def get_queryset(self):
-        queryset = Prediction.objects.all().order_by("data_hora","trip_short_name", "stop_id")
+        queryset = Prediction.objects.all().order_by("dataHora","trip_short_name", "stop_id")
 
         # filtrar por trip_name (trip_short_name)
         trip_name = self.request.query_params.get("trip_name")
@@ -26,10 +26,13 @@ class PredictorViewSet(viewsets.ModelViewSet):
             trip_name = trip_name.split(",")
             queryset = queryset.filter(trip_short_name__in=trip_name)
 
-        # filtrar por stop_id
-        stop_id = self.request.query_params.get("stop_id")
-        if stop_id:
-            stop_id = stop_id.split(",")
-            queryset = queryset.filter(stop_id__in=stop_id)
+        # calcular tempo de viagem até stop_id_destiny
+        stop_id_destiny = self.request.query_params.get("stop_id_destiny")
+        if stop_id_destiny:
+            # 1.ordenando por 
+            stop_id_destiny = stop_id_destiny.split(",")
+            # queryset = queryset.filter(stop_id__in=stop_id_destiny)
+
+        
 
         return queryset
