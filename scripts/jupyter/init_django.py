f31c8adf90bb98e1fbb53e04c24ef483ea14ab8a
Raphael Rivas <raphaelrivas@hotmail.com>
2023-02-01 03:47:23 -0300

    tests for predictor viewset

--- scripts/jupyter/init_django.py
+++ scripts/jupyter/init_django.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import dotenv
+
 # set relative path to server's root
 sys.path.append('../../mobilidade_rio')
 # ls
@@ -7,6 +9,14 @@ sys.path.append('../../mobilidade_rio')
 # set variables
 os.environ["DJANGO_SETTINGS_MODULE"] = "mobilidade_rio.settings.native"
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+
+# load env files
+# print if env exists
+ENV_PATH = '../../../mobilidade_rio/dev_local/api.env'
+script_path = os.path.abspath(__file__)
+ENV_PATH = os.path.abspath(os.path.join(script_path, ENV_PATH))
+dotenv.load_dotenv(dotenv_path=ENV_PATH)
+
 # start django
 import django
 django.setup()
