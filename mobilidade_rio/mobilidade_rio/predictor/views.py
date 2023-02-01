13dcc883be0ba60152182a37f06ff90469a7f29e
Raphael Rivas <raphaelrivas@hotmail.com>
2023-02-01 03:46:02 -0300

    (tested) working predictor viewset

--- mobilidade_rio/mobilidade_rio/predictor/views.py
+++ mobilidade_rio/mobilidade_rio/predictor/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets
 from mobilidade_rio.predictor.models import Prediction
 from mobilidade_rio.predictor.serializers import PredictionSerializer
-# import APIView
+import django.db.models as dm
+import pandas as pd
 
 
 class PredictorViewSet(viewsets.ModelViewSet):
@@ -19,7 +20,8 @@ class PredictorViewSet(viewsets.ModelViewSet):
     serializer_class = PredictionSerializer
 
     def get_queryset(self):
-        queryset = Prediction.objects.all().order_by("dataHora","trip_short_name", "stop_id")
+        queryset = Prediction.objects.all().order_by(
+            "dataHora", "trip_short_name", "stop_id")
 
         # filtrar por trip_name (trip_short_name)
         trip_name = self.request.query_params.get("trip_name")
@@ -30,10 +32,19 @@ class PredictorViewSet(viewsets.ModelViewSet):
         # calcular tempo de viagem até stop_id_destiny
         stop_id_destiny = self.request.query_params.get("stop_id_destiny")
         if stop_id_destiny:
-            # 1.ordenando por 
             stop_id_destiny = stop_id_destiny.split(",")
-            # queryset = queryset.filter(stop_id__in=stop_id_destiny)
-
-        
+            # TODO: 1. Order by stop_sequence (doesn't exist)
+            #       Solutions: create col; order by pk (need rewrite tasks.py?)
+            # queryset = queryset.order_by("pk")
+            # 2. Selecionar da linha que contém id_veiculo
+            vehicles = queryset.filter(id_veiculo__isnull=False, arrival_time__isnull=False)
+            # # 3. Para cada veículo, selecionar paradas posteriores e somar arrival_time
+            df_vehicles = pd.DataFrame.from_records(vehicles.values())
+            for i, vehicle in enumerate(vehicles):
+                after_vehicle = queryset.filter(
+                    pk__gt=vehicle.pk, trip_id=vehicle.trip_id, arrival_time__isnull=False)
+                sum_after = list(after_vehicle.aggregate(dm.Sum("arrival_time")).values())[0]
+                df_vehicles.loc[i, "arrival_time"] = vehicle.arrival_time + sum_after
+            queryset = [dict(zip([col for col in df_vehicles], row)) for row in df_vehicles.values]
 
         return queryset
