39473c828e1b053298f065251cf6d999da043b13
Raphael Rivas <raphaelrivas@hotmail.com>
2023-03-14 00:27:35 -0300

    fix: cronjob to update predictions in cache
    refs: 116
    
    commit fc8b71ba327c33ccda4180415415f3eb9a8a36a1
    Author: Raphael Rivas <raphaelrivas@hotmail.com>
    Date:   Tue Mar 14 00:12:13 2023 -0300
    
        fix: django_q 2
    
    commit 3a8cfca4922c5f9fb01c0acc20d9448481869418
    Author: Raphael Rivas <raphaelrivas@hotmail.com>
    Date:   Mon Mar 13 23:59:23 2023 -0300
    
        fix: django_q
    
    commit 96aacaa8dbd8ae77cce13954fd16a5a0c995fe25
    Author: Raphael Rivas <raphaelrivas@hotmail.com>
    Date:   Mon Mar 13 23:32:29 2023 -0300
    
        fix: predictor dont get stuck to update prediction
    
        refs: Criar endpoint do preditor #116
    
    commit e719d9fefb68dbadc418f9f54e133d2029703b7d
    Author: Raphael Rivas <raphaelrivas@hotmail.com>
    Date:   Mon Mar 13 14:48:22 2023 -0300
    
        feat: predictor show all results, takes ~25s
    
        It shows bus with negative eta (passed after stop)
        refs: #116
    
    commit 3259f1ee11391c607a75070e098a04a0d3788ec8
    Author: Fernanda Scovino <fscovinom@gmail.com>
    Date:   Mon Mar 13 12:16:06 2023 -0300
    
        add predictor for all stops
    
    commit 470d29c52ff74c8db75caa7886bedbd3f9a7d9f0
    Author: Fernanda Scovino <fscovinom@gmail.com>
    Date:   Fri Mar 10 14:03:19 2023 -0300
    
        Refatora código do preditor (#158)
    
        * correct timezone
    
        * refactor predictor
    
        * fix black format
    
        * set minimum speed
    
        * final fixes
    
        * refactor, fix: predictor class
    
        ---------
    
        Co-authored-by: Raphael Rivas <raphaelrivas@hotmail.com>

--- mobilidade_rio/mobilidade_rio/predictor/views.py
+++ mobilidade_rio/mobilidade_rio/predictor/views.py
@@ -5,7 +5,9 @@ from rest_framework.response import Response
 from mobilidade_rio.predictor.models import *
 from mobilidade_rio.predictor.serializers import *
 from mobilidade_rio.predictor.utils import *
-
+from mobilidade_rio.predictor.models import PredictorResult
+import json
+from django.core import serializers
 
 class PredictorViewSet(viewsets.ViewSet):
     """
@@ -19,40 +21,33 @@ class PredictorViewSet(viewsets.ViewSet):
         """
 
         # stop_code
-        stop_code = self.request.query_params.get("stop_code")
-        if stop_code:
-            stop_code = stop_code.split(',')
+        # stop_id = self.request.query_params.get("stop_id")
+        # if not stop_id:
+        #     return Response({"error": "stop_id is required."})
 
         # trip_short_name
-        trip_short_name = self.request.query_params.get("trip_short_name")
-        if trip_short_name:
-            trip_short_name = trip_short_name.split(',')
+        # trip_short_name = self.request.query_params.get("trip_short_name")
+        # if trip_short_name:
+        #     trip_short_name = trip_short_name.split(',')
 
-        # direction_id
-        direction_id = self.request.query_params.get("direction_id")
-        if direction_id:
-            direction_id = stop_code.split(',')
+        # direction_id = self.request.query_params.get("direction_id")
+        # if direction_id:
+        #     direction_id = stop_id.split(',')
 
         # debug_cols
-        debug_cols = self.request.query_params.get("debug_cols")
+        # debug_cols = self.request.query_params.get("debug_cols")
 
         # Run predictor
-        pred = Predictor(
-            stop_code=stop_code,
-            direction_id=direction_id,
-            trip_short_name=trip_short_name,
-            debug_cols=debug_cols
-        )
-        ret = pred.get_eta()
-
-        if ret:
-            ret = {
-                "count": len(ret),
+        results = PredictorResult.objects.filter(pk=1)
+        if results.exists():
+            results = results[0].result_json['result']
+            results = {
+                "count": len(results),
                 "next": None,
                 "previous": None,
-                "results" : ret
+                "results" : results
             }
         else:
-            ret = {"detail": "Not found."}
+            results = {"detail": "Not found."}
 
-        return Response(ret)
+        return Response(results)
