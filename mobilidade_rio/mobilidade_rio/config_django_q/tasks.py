75b1808d51faf5245955f2c8654d6b17c19b6f48
Raphael Rivas <raphaelrivas@hotmail.com>
2023-03-17 13:52:57 -0300

    fix: reactivate job run prediction every 30s

--- mobilidade_rio/mobilidade_rio/config_django_q/tasks.py
+++ mobilidade_rio/mobilidade_rio/config_django_q/tasks.py
@@ -37,13 +37,18 @@ def generate_prediction():
 # TODO: Decide if this function will recall itself every 20 seconds 3x or if apps.py will do it.
 def generate_prediction_sleep(wait_secs=30):
     start = dt.now()
-    logger.info("starting job 1 - generating prediction")
+    logger.info("starting job 1.a - generating prediction")
     generate_prediction()
-    
-    diff = (dt.now() - start).total_seconds()
-    # if diff < wait_secs:
-    #     time.sleep(wait_secs - diff)
 
+    diff = (dt.now() - start).total_seconds()
     logger.info(f"This prediction took {diff}s")
-    # generate_prediction()
+
+    if diff > 60:
+        logger.info("Aborting job 1.b, 1.a took > 60s")
+        return
+    if diff < wait_secs:
+        time.sleep(wait_secs - diff)
+
+    logger.info("starting job 1.b - generating prediction")
+    generate_prediction()
     logger.info("finished job")
