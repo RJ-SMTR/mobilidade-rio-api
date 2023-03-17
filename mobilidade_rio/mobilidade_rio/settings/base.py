486f55b9fa035cb11a59f7d88c1be67ca292538f
Raphael Rivas <raphaelrivas@hotmail.com>
2023-03-17 13:23:01 -0300

    fix: job taking too long - recycle 5

--- mobilidade_rio/mobilidade_rio/settings/base.py
+++ mobilidade_rio/mobilidade_rio/settings/base.py
@@ -167,6 +167,7 @@ Q_CLUSTER = {
     'retry': 60*12,
     # Number of messages each cluster tries to get from the broker per call
     'bulk': 1,
+    "recycle": 5,
     'workers': 1,
     'queue_limit': 1,
     # Explicittly don't run deleted tasks
