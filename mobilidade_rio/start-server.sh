fd8a300508a70e2998930826782aba391615f9e8
Caio R S dos Santos <caiorogerio.santos@gmail.com>
2023-03-28 13:07:31 -0300

    [FIX] Separa instâncias da api com e sem DjangoQ ativado (#162)

--- mobilidade_rio/start-server.sh
+++ mobilidade_rio/start-server.sh
@@ -3,5 +3,5 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
   (cd /app; python manage.py createsuperuser --no-input)
 fi
-(cd /app; python manage.py qcluster & gunicorn mobilidade_rio.wsgi --user www-data --bind 0.0.0.0:8000 --workers 3) &
+(cd /app; gunicorn mobilidade_rio.wsgi --user www-data --bind 0.0.0.0:8000 --workers 3) &
 nginx -g "daemon off;"
