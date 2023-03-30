6eaa318f83f31e2070e8d913a12450f7db8d2e70
Fernanda Scovino <fscovinom@gmail.com>
2023-03-30 14:24:31 -0300

    aumenta page_size

--- mobilidade_rio/mobilidade_rio/settings/base.py
+++ mobilidade_rio/mobilidade_rio/settings/base.py
@@ -125,7 +125,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 100,
 }
 
 # djoser
