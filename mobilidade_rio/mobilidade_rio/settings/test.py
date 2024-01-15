cbeadeee403ba4908d8726874985411cc05627b1
Raphael Rivas <raphaelrivas@hotmail.com>
2024-01-15 11:25:49 -0300

    ci: use test settings

--- mobilidade_rio/mobilidade_rio/settings/test.py
+++ mobilidade_rio/mobilidade_rio/settings/test.py
@@ -1,20 +1,12 @@
-from os import getenv
 from .base import *
-
-# Allowed hosts
-ALLOWED_HOSTS = ["*"]
-
-# CORS
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': getenv("DB_NAME"),
-        'USER': getenv("DB_USER"),
-        'PASSWORD': getenv("DB_PASSWORD"),
-        'HOST': getenv("DB_HOST"),
-        'PORT': getenv("DB_PORT"),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testdb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
