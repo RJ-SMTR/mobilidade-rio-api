7ef463048269961164f4b4b21dedca9d4da83214
Raphael Rivas <raphaelrivas@hotmail.com>
2024-03-29 23:16:46 -0300

    fix: remove hardcoded example secret

--- mobilidade_rio/mobilidade_rio/settings/local.py
+++ mobilidade_rio/mobilidade_rio/settings/local.py
@@ -10,7 +10,7 @@ DATABASES = {
         "ENGINE": getenv("DB_ENGINE", "django.db.backends.postgresql_psycopg2"),
         "NAME": getenv("DB_NAME", "postgres"),
         "USER": getenv("DB_USER", "postgres"),
-        "PASSWORD": getenv("DB_PASSWORD", "postgres"),
+        "PASSWORD": getenv("DB_PASSWORD"),
         "HOST": getenv("DB_HOST", "localhost"),
         "PORT": getenv("DB_PORT", "5434"),
     }
